5406cd60914de5b59f51fa295a76e1ffdad961f7
Rendy Febry <rendyfebry@gmail.com>
2018-07-24 17:35:29 +0700

    Setup Simple Navigation

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,32 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <form>
-            <input type="text" name="email" placeholder="Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <button type="submit">Login</button>
-        </form>
+    <div class="container">
+        <div class="wrapper">
+            <h2>Login</h2>
+            <form>
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" />
+                </div>
 
-        <hr />
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Password" />
+                </div>
 
-        <button type="button" @click="googleLogin">Continue with Google</button>
-        <button type="button" @click="facebookLogin">Continue with Facebook</button>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">
+                        Login
+                    </button>
+                </div>
+            </form>
+
+            <hr />
+
+            <button type="button" class="btn btn-primary btn-block" @click="googleLogin">
+                Continue with Google
+            </button>
+            <button type="button" class="btn btn-primary btn-block" @click="facebookLogin">
+                Continue with Facebook
+            </button>
+        </div>
     </div>
 </template>
 
@@ -27,3 +43,11 @@ export default {
 	},
 }
 </script>
+
+<style scoped>
+.wrapper {
+	max-width: 400px;
+	margin: 0 auto;
+}
+</style>
+
