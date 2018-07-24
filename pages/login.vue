2285322e7cb91a076def137daf7ad6e70ccc39f8
Rendy Febry <rendyfebry@gmail.com>
2018-07-24 17:15:29 +0700

    Setup profile page & login page

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <div>
+            <pre>{{ state }}</pre>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	middleware: ['auth'],
+}
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Profile</h2>
+        <div>
+            <pre>{{ state }}</pre>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	middleware: ['auth'],
+	computed: {
+		state() {
+			return JSON.stringify(this.$auth.$state, undefined, 2)
+		},
+	},
+}
+</script>
