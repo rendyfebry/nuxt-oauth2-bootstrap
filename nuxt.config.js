908cb2ce6a2aac227a67bbe88304af2ed2094b2f
Rendy Febry <rendyfebry@gmail.com>
2018-07-24 17:28:00 +0700

    Setup bootstrap & Change default font to Lato

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,20 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Alkemi - Dead simple page builder' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css',
+      },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900' },
+      { rel: 'stylesheet', href: '/main.css' },
+    ],
   },
   /*
   ** Customize the progress bar color
